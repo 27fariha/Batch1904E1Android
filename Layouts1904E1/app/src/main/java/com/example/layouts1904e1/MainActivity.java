5e63b1813e27f6d1385591859c0bab6619853eee
fariha <fariha_ahmed27@hotmail.com>
2021-01-29 16:48:23 +0500

    first commit

---
+++ Layouts1904E1/app/src/main/java/com/example/layouts1904e1/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.layouts1904e1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
